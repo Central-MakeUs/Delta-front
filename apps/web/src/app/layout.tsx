8ff7ad0e4e9ba98eebde6a73574dfd113bc917ca
Dubabbi <thdms3160@naver.com>
2025-12-29 23:38:02 +0900

    refactor: QueryProvider 네이밍 통일

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import QueryProviders from "@/shared/utils/query-provider";
+import QueryProvider from "@/shared/utils/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <QueryProviders>{children}</QueryProviders>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );

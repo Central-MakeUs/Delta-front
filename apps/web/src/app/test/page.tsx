c30f8d535a1ae8f15cea600d7483232ddca4ecad
kimkimjunjun <wnsgnl323@gmail.com>
2025-12-31 00:07:36 +0900

    chore: react-compiler 초기 세팅

---
+++ apps/web/src/app/test/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useState } from "react";
+
+const TestComponent = () => {
+    console.log("ChildComponent rendered");
+    return <div>TestComponent</div>;
+  }
+
+const TestPage = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    console.log("Button clicked");
+    setCount(count + 1);
+  };
+
+  console.log("ParentComponent rendered");
+
+  return (
+    <>
+      <div className="card">
+        <button onClick={handleClick}>count is {count}</button>
+      </div>
+      <TestComponent />
+    </>
+  );
+}
+
+export default TestPage;

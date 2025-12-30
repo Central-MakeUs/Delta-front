0e00469335dcc299f37926afc5863ef9ea0238f0
kimkimjunjun <wnsgnl323@gmail.com>
2025-12-30 23:33:36 +0900

    chore: Vanilla기반 Storybook 세팅

---
+++ apps/web/.storybook/preview.ts
@@ -0,0 +1,20 @@
+import type { Preview } from '@storybook/nextjs';
+import React from 'react';
+import '../src/shared/styles/index.css';
+import { lightTheme } from '../src/shared/styles/theme.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => React.createElement('div', { className: lightTheme }, React.createElement(Story)),
+  ],
+};
+
+export default preview;

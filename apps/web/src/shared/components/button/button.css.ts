b0a7aad52baacdf57c5ba789077a5f80e0d056e7
kimkimjunjun <wnsgnl323@gmail.com>
2026-01-03 17:51:12 +0900

    chore: typo, color 토큰

--- apps/web/src/shared/components/button/button.css.ts
+++ apps/web/src/shared/components/button/button.css.ts
@@ -1,14 +1,15 @@
 import { recipe } from "@vanilla-extract/recipes";
 import { vars } from "@/shared/styles/theme.css";
 import { typo } from "@/shared/styles/typography.css";
+import { color } from "@/shared/styles/color.css";
 
 export const button = recipe({
   base: [
     typo.button1,
+    color["main-500"],
+    color["mainBg-500"],
     {
       border: "none",
-      backgroundColor: vars.color.main[500],
-      color: "#fff",
       borderRadius: vars.radius.r12,
     },
   ],

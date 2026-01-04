40ec7f2816d4dd16d5fcdebb1939e31d88c7438e
Dubabbi <thdms3160@naver.com>
2026-01-04 15:59:38 +0900

    style: fab 수정

--- apps/web/src/shared/components/button/fab-button/fab-button.css.ts
+++ apps/web/src/shared/components/button/fab-button/fab-button.css.ts
@@ -14,28 +14,6 @@ export const fabButton = style({
   justifyContent: "center",
   color: vars.color.grayscale[0],
   cursor: "pointer",
-  touchAction: "manipulation",
-
-  selectors: {
-    "&:hover:not(:disabled)": {
-      backgroundColor: vars.color.main[600],
-    },
-    "&:active:not(:disabled)": {
-      backgroundColor: vars.color.main[700],
-    },
-
-    "&:focus-visible": {
-      outline: "none",
-      boxShadow: `0 0 0 0.3rem ${vars.color.main[100]}, ${vars.shadow.e400}`,
-    },
-
-    "&:disabled": {
-      cursor: "not-allowed",
-      backgroundColor: vars.color.grayscale[100],
-      color: vars.color.grayscale[500],
-      boxShadow: "none",
-    },
-  },
 });
 
 export const icon = style({

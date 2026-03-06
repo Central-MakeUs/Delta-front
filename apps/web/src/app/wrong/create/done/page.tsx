f84f69f234524b6d48e2126887110d461c1d7fba
Dubabbi <thdms3160@naver.com>
2026-03-06 23:49:22 +0900

    style: 오답 생성 완료 페이지 수정

--- apps/web/src/app/wrong/create/done/page.tsx
+++ apps/web/src/app/wrong/create/done/page.tsx
@@ -24,22 +24,24 @@ const WrongCreateDonePage = () => {
     <div className={s.page}>
       <DoneIllustration />
 
-      <div className={s.textBlock}>
-        <h2 className={clsx(typo.h2, color["grayscale-900"])}>
-          문제가 등록됐어요.
-        </h2>
-        <p className={clsx(typo.body1.medium, color["grayscale-600"])}>
-          이제 천천히 오답을 정리해볼까요?
-        </p>
-      </div>
+      <div className={s.bottomSection}>
+        <div className={s.textBlock}>
+          <h2 className={clsx(typo.h2, color["grayscale-900"])}>
+            문제가 등록됐어요.
+          </h2>
+          <p className={clsx(typo.body1.medium, color["grayscale-600"])}>
+            이제 천천히 오답을 정리해볼까요?
+          </p>
+        </div>
 
-      <div className={s.actions}>
-        <Button
-          tone="dark"
-          label="문제 계속 등록하기"
-          onClick={handleGoCreate}
-        />
-        <Button tone="default" label="홈으로" onClick={handleGoHome} />
+        <div className={s.actions}>
+          <Button
+            tone="dark"
+            label="문제 계속 등록하기"
+            onClick={handleGoCreate}
+          />
+          <Button tone="default" label="홈으로" onClick={handleGoHome} />
+        </div>
       </div>
     </div>
   );
